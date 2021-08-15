3ec50e66f5d5cac6387a0f3bf04c9b17a567d428
codestorms37 <46337583+codestorms37@users.noreply.github.com>
2021-08-14 23:17:31 -0500

    Four routes completed

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify, render_template
 
-
+# The conection shouldn´t be inside the functions in order to open and close as soon as possible??????????????????
 
 # setup database
 # The create_engine() function allows us to access and query our SQLite database file.
@@ -27,7 +27,7 @@ Measurement = Base.classes.measurement
 Station = Base.classes.station
 
 # create a session link from Python to our database
-session = Session(engine)
+# session = Session(engine)
 
 
 
@@ -71,8 +71,70 @@ def welcome():
 @app.route("/api/v1.0/precipitation")
 
 def precipitation():
+
+    session = Session(engine)
+
     prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
     precipitation = session.query(Measurement.date, Measurement.prcp).\
         filter(Measurement.date >= prev_year).all()
-    precip = {date: prcp for date, prcp in precipitation}
+    precip = {date: prcp for date, prcp in precipitation} # I dont understand this ??????????????
     return jsonify(precip)
+
+# 9.5.4 Stations Route
+
+@app.route("/api/v1.0/stations")
+
+def stations():
+
+    session = Session(engine)
+
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results)) # What is ravel??????????????????
+    return jsonify(stations=stations) # stations=stations ?????????????????????????????
+
+
+# 9.5.5 Monthly Temperature Route
+
+@app.route("/api/v1.0/tobs")
+
+def temp_monthly():
+
+    session = Session(engine)
+
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+        filter(Measurement.date >= prev_year).all()
+    
+    #print(results)
+    
+    temps = list(np.ravel(results))
+    
+    return jsonify(temps=temps)
+
+
+# 9.5.6 Statistics Route
+
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+
+def stats(start=None, end=None):
+
+    session = Session(engine)
+
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+
+    temps = list(np.ravel(results))
+
+    return jsonify(temps)

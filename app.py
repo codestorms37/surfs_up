ba206dd56d61391661b1108c412f85ef80af7ed2
codestorms37 <46337583+codestorms37@users.noreply.github.com>
2021-08-14 22:33:26 -0500

    Precipitation route created
    Added import render_template

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 
 
@@ -35,3 +35,44 @@ session = Session(engine)
 # create a Flask application called "app"
 app = Flask(__name__)
 
+# Important Note:
+myNote = '''
+
+Notice the __name__ variable in this code.
+This is a special type of variable in Python.
+Its value depends on where and how the code is run.
+For example, if we wanted to import our app.py file into another Python file named example.py,
+the variable __name__ would be set to example. Here's an example of what that might look like:
+
+import app
+
+print("example __name__ = %s", __name__)
+
+if __name__ == "__main__":
+    print("example is being run directly.")
+else:
+    print("example is being imported")
+
+However, when we run the script with python app.py, 
+the __name__ variable will be set to __main__. 
+This indicates that we are not using any other file to run this code.
+
+'''
+
+# 9.5.2 Create the Welcome Route
+
+@app.route("/")
+
+def welcome():
+    return render_template('welcome.html')
+
+# 9.5.3 Precipitation Route
+
+@app.route("/api/v1.0/precipitation")
+
+def precipitation():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)

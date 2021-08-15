52c81a04d0732b512c459305aeb8ef5d85fa9d5e
codestorms37 <46337583+codestorms37@users.noreply.github.com>
2021-08-14 21:33:25 -0500

    app.py initial definition
    hello_world.py created
    app.py redefined

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
+# 9.5.1 Set Up the Database and Flask
 
-from flask import Flask
+# Dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+
+# setup database
+# The create_engine() function allows us to access and query our SQLite database file.
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect the database into our classes.
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+# save our references to each table.
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# create a session link from Python to our database
+session = Session(engine)
+
+
+
+# Set Up Flask
+# create a Flask application called "app"
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello world'
-
-@app.route('/d/')
-def d():
-    return 'd'
